23528f9693a77083aeac5731287d8756669e75fb
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-09-25 09:23:46 +0300

    Добавьте файлы проекта.

---
+++ Services/PasswordHasherService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+
+namespace Shop.PasswordHasher.Services
+{
+    public class PasswordHasherService : PasswordHasher.PasswordHasherBase
+    {
+        public override Task<PasswordHashReply> GeneratePasswordHash(PasswordRequest password, ServerCallContext context)
+        {
+            var passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password.Password);
+            return Task.FromResult(new PasswordHashReply
+            {
+                PasswordHash = passwordHash
+            });
+        }
+
+        public override Task<PasswordVerificationReply> VerifyPassword(PasswordVerificationRequest request, ServerCallContext context)
+        {
+            var isPasswordVerify = BCrypt.Net.BCrypt.Verify(request.Password, request.HashedPassword);
+            return Task.FromResult(new PasswordVerificationReply
+            {
+                IsPasswordVerify = isPasswordVerify.ToString()
+            });
+        }
+    }
+}
